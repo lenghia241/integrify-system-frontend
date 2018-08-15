2a345a381f80cd524110e67b476114f4c01dd52a
trungh13 <newbie.trungh13@gmail.com>
2018-08-15 16:49:26 +0300

    add eslint/csslint, router, redux ( form / thunk)

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Attendance from './containers/Attendance';
+import Profile from './containers/Profile';
+import Dashboard from './containers/Dashboard';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Switch>
+        <Route exact path="/attendance" component={Attendance}/>
+        <Route exact path="/profile" component={Profile}/>
+        <Route exact path="/" component={Dashboard}/>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
