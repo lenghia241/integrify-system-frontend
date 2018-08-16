94e5692d894be026fa180a9e758233db93529315
trungh13 <newbie.trungh13@gmail.com>
2018-08-16 15:25:55 +0300

    add Redux DEVTOOL

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import reducers from './store/reducers';
-
-const store = createStore(reducers, {});
-
+/*eslint-disable */
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+/* eslint-enable */
 ReactDOM.render(
   <Provider store={store}>
     <Router>
