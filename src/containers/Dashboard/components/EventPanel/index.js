bbec377894fd66fc89e30d0d6156f10e583e6ecb
Abdi Muleta <abdi.muleta@gmail.com>
2018-08-20 18:00:01 +0300

    eventspanel component cleaned

--- src/containers/Dashboard/components/EventPanel/index.js
+++ src/containers/Dashboard/components/EventPanel/index.js
@@ -15,12 +15,14 @@ class EventPanel extends Component {
             <div className="container">
                 <h1>Upcoming Events</h1>
                 <ul className="collapsible">
-                    { events.map(({_id, title, description, time}) => (
+                    { events.map(({_id, title, description, venue, time, event_url}) => (
                         <li key= {_id}>
-                            <div className="collapsible-header">{title}</div>
+                            <div className="collapsible-header"><h5>Event title: {title}</h5></div>
                             <div className="collapsible-body">
-                            <p>{description}</p>
-                            <p className="red-text"><i className="material-icons">calendar</i>{time}</p>
+                                <p>Event description: {description}</p>
+                                <p className="black-text"><i className="material-icons">event_available</i>{time}</p>
+                                <p className="black-text"><i className="material-icons">location_on</i>{venue.city}</p>
+                                <p className="black-text"><i className="material-icons">web</i><a href="">{event_url}</a></p>
                             </div>
                         </li>
                     ))}
