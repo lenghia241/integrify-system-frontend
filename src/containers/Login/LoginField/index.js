283278399e9e7c355dfd627fc29b0a275b7bfdac
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-08-21 17:21:45 +0300

    Login Field component for redux-form

---
+++ src/containers/Login/LoginField/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/* eslint jsx-a11y/label-has-for: 0 */
+const LoginField = ({ input = {}, label, meta: { error, touched, active } }) => {
+  let activeClass = '';
+
+  if (input.value !== '') activeClass = 'active';
+  else if (active) activeClass = 'active';
+  else activeClass = '';
+
+  return (
+    <div className="input-field">
+      <label htmlFor={input.name} className={activeClass}>
+        {label}
+      </label>
+      <input {...input} type={input.type} name={input.name} />
+    </div>
+  );
+};
+
+LoginField.propTypes = {
+  input: PropTypes.shape({}).isRequired,
+  label: PropTypes.string.isRequired,
+  meta: PropTypes.shape({
+    touched: PropTypes.bool,
+    error: PropTypes.string,
+    active: PropTypes.bool,
+  }),
+};
+
+LoginField.defaultProps = {
+  meta: {
+    touched: false,
+    error: '',
+    active: false,
+  },
+};
+export default LoginField;
