641cc8fe1d8ce66ebaea5d3b89de382163640c81
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-08-22 18:43:28 +0300

    Login page styles implemented

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -4,37 +4,41 @@ import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 import LoginField from './LoginField';
 
+import './Login.css';
+
 const Login = ({ handleSubmit }) => (
-  <div
-    style={{
-      background:
-        'url("https://s3.eu-west-2.amazonaws.com/integrify-system-assets/background-img.jpg") no-repeat center center fixed',
-      backgroundSize: 'cover',
-    }}
-  >
-    <div className="section container">
+  <div className="background">
+    <div className="section container" style={{ transform: 'translateY(40%)' }}>
       <div className="row">
         <div className="col s12 m6 offset-m3">
           <div className="card card-wrapper">
             <div className="card-content">
-              <h5 className="center">Login</h5>
+              <div className="logo-container">
+                <img
+                  src="https://s3.eu-west-2.amazonaws.com/integrify-system-assets/logo-black.png"
+                  alt="logo"
+                  className="logo"
+                />
+              </div>
               <form onSubmit={handleSubmit}>
                 <Field name="email" component={LoginField} type="email" label="Email" />
                 <Field name="password" component={LoginField} type="password" label="Password" />
-                <button type="submit" className="btn">
-                  Submit
-                </button>
-                <Link
-                  style={{
-                    display: 'block',
-                    fontSize: '14px',
-                    marginTop: '15px',
-                  }}
-                  to="/forgot-password/"
-                >
-                  Forgot Password?
-                </Link>
+                <small>
+                  <Link to="/forgot-password/" className="right-align forgot-pw">
+                    Forgot your password?
+                  </Link>
+                </small>
+                <div className="center">
+                  <button type="submit" className="btn">
+                    Submit
+                  </button>
+                </div>
               </form>
+            </div>
+            <div className="card-content grey lighten-4 center-align">
+              <p>
+                Don&apos;t have an account? <Link to="/signup">Sign up now!</Link>
+              </p>
             </div>
           </div>
         </div>
