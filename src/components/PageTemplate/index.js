e858083468cfbc26d337f40b4f16161d8449e641
trungh13 <newbie.trungh13@gmail.com>
2018-08-23 21:35:37 +0300

    add pagetemplate

---
+++ src/components/PageTemplate/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import SearchBar from './SearchBar';
+import './PageTemplate.css';
+
+const pageTemplate = ({ children, heading }) => (
+  <React.Fragment>
+    <div className="page-heading">
+      <h1>{heading}</h1>
+      <SearchBar />
+    </div>
+    {children}
+  </React.Fragment>
+);
+
+pageTemplate.propTypes = {
+  heading: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
+};
+
+export default pageTemplate;
