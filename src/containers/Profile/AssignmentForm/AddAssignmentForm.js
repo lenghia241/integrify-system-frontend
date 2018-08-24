e347fc6356ed6677b0855c8c0db9514b62ffc40b
Yasir Akbar <yasir269050@gmail.com>
2018-08-24 13:58:07 +0300

    Regex Validation added

--- src/containers/Profile/AssignmentForm/AddAssignmentForm.js
+++ src/containers/Profile/AssignmentForm/AddAssignmentForm.js
@@ -8,39 +8,51 @@ const validate = values => {
 
   if (!values.assignment) {
     errors.assignment = "Required";
+  } else if (!/^[A-Za-z0-9 _]*[A-Za-z0-9][A-Za-z0-9 _]*$/i.test(values.assignment)) {
+    errors.assignment = 'Invalid assignment name'
   }
 
   if (!values.github) {
     errors.github = "Required";
+  } else if (!/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()\\+,;=.]+$/i.test(values.github)) {
+    errors.github = 'Invalid github Link'
   }
+  
   if (!values.status) {
     errors.status = "Required";
   }
 
   if (!values.teacher) {
     errors.teacher = "Required";
+  } else if (!/^[a-zA-Z\s]+$/i.test(values.teacher)) {
+    errors.teacher = 'Invalid name'
   }
 
   return errors;
 };
-const renderField = ({ input, label, type, meta: { touched, error } }) => {
-  return (
+
+const renderField = ({
+
+  input, label, type, meta: { touched, error },
+
+}) => (
+
     <div>
-      {" "}
+
+      {' '}
+
       <div>
+
         {touched && (error && <span className="error">{error}</span>)}
-        <input {...input} type={type} />{" "}
+
+        <input {...input} type={type} />{' '}
+
       </div>
-      <div>
-        <input
-          type={type}
-          checked={input.value ? true : false}
-          onChange={(e, { checked }) => input.onChange(checked)}
-        />
-      </div>
+
     </div>
-  );
-};
+
+);
+
 
 const AddAssignmentForm = props => {
   const { handleSubmit, pristine, reset, submitting } = props;
