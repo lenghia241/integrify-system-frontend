80ec0e250256d5d700c43c84c4d2871a3f39548a
Katarina Khan <only4katka@gmail.com>
2018-08-29 01:10:18 +0300

    refactored code to avoid repetitions

--- src/containers/Profile/ProfileForm/ProfileFormSummary.js
+++ src/containers/Profile/ProfileForm/ProfileFormSummary.js
@@ -6,15 +6,19 @@ import RenderSummary from './render/RenderSummary';
 
 const ProfileFormSummary = (props) => {
   const { values } = props;
-  console.log(props);
   const content = [
     {
       className: 'personal',
       title: 'Personal Information',
       fields: [
-        { title: 'First Name', value: values && values.firstName ? values.firstName : null },
-        { title: 'Last Name', value: values && values.lastName ? values.lastName : null },
-        { title: 'Bio', value: values && values.bio ? values.bio : null },
+        [
+          {
+            title: 'First Name',
+            value: values && values.firstName ? values.firstName : null,
+          },
+          { title: 'Last Name', value: values && values.lastName ? values.lastName : null },
+          { title: 'Bio', value: values && values.bio ? values.bio : null },
+        ],
       ],
     },
     {
@@ -44,49 +48,69 @@ const ProfileFormSummary = (props) => {
     {
       className: 'education',
       title: 'Education',
-      fields: [
-        {
-          title: 'School',
-          value:
-            values && values.education && values.education[0] !== undefined
-              ? values.values.education[0].school
-              : null,
-        },
-        {
-          title: 'Degree',
-          value:
-            values && values.education && values.education[0] !== undefined
-              ? values.lastName
-              : null,
-        },
-        { title: 'Bio', value: values && values.bio ? values.bio : null },
-      ],
-    },
-    {
-      className: 'education',
-      title: 'Education',
       fields:
         values && values.education && values.education[0] !== undefined
-          ? values.education.map((item, i) => ({
-            school: {
+          ? values.education.map((item, i) => [
+            {
               title: `School #${i + 1}`,
               value: item.school,
             },
-            degree: { title: `Degree #${i + 1}`, value: item.degree },
-            fieldofstudy: {
+            { title: `Degree #${i + 1}`, value: item.degree },
+            {
               title: `Field of study #${i + 1}`,
               value: item.fieldofstudy,
             },
-            from: {
+            {
               title: `From #${i + 1}`,
               value: item.from,
             },
-            to: {
+            {
               title: `To #${i + 1}`,
               value: item.to,
             },
-            description: { title: `Description #${i + 1}`, value: item.descriptopn },
-          }))
+            { title: `Description #${i + 1}`, value: item.descriptopn },
+          ])
+          : null,
+    },
+    {
+      className: 'examplesofwork',
+      title: 'Examples of Work',
+      fields:
+        values && values.examplesofwork && values.examplesofwork[0] !== undefined
+          ? values.examplesofwork.map((item, i) => [
+            {
+              title: `Title #${i + 1}`,
+              value: item.title,
+            },
+            { title: `Github link #${i + 1}`, value: item.github },
+          ])
+          : null,
+    },
+    {
+      className: 'experience',
+      title: 'Experience',
+      fields:
+        values && values.workexperience && values.workexperience[0] !== undefined
+          ? values.workexperience.map((item, i) => [
+            {
+              title: `Title #${i + 1}`,
+              value: item.title,
+            },
+            { title: `Company #${i + 1}`, value: item.company },
+            {
+              title: `Location #${i + 1}`,
+              value: item.location,
+            },
+            {
+              title: `From #${i + 1}`,
+              value: item.from,
+            },
+            {
+              title: `To #${i + 1}`,
+              value: item.to,
+            },
+            { title: `Description #${i + 1}`, value: item.descriptopn },
+          ])
           : null,
     },
     {
@@ -98,65 +122,17 @@ const ProfileFormSummary = (props) => {
           : null,
     },
   ];
-  console.log(content);
+
   return (
     <div className="summary">
-      {content.map((item, i) => (
+      {content.map(item => (
         <RenderSummary
-          key={`${i + 1}`}
+          key={`${item.title}`}
           className={item.className}
           title={item.title}
           fields={item.fields}
         />
       ))}
-      <div className="examplesofwork">
-        <h5>Examples of Work</h5>
-        {values && values.examplesofwork && values.examplesofwork[0] !== undefined
-          ? values.examplesofwork.map((item, i) => (
-              <div key={`${i + 1}`}>
-                <p key={`${i + 1}`}>Title</p>
-                {item.title ? <p key={`${i + 1}`}>{values.examplesofwork[i].title}</p> : null}
-                <p key={`${i + 1}`}>Github link</p>
-                {values.examplesofwork[i].github ? (
-                  <p key={`${i + 1}`}>{values.examplesofwork[i].github}</p>
-                ) : null}
-              </div>
-          ))
-          : null}
-      </div>
-      <div className="experience">
-        <h5>Experience</h5>
-        {values && values.workexperience && values.workexperience[0] !== undefined
-          ? values.workexperience.map((item, i) => (
-              <div key={`${i + 1}`}>
-                <p key={`${i + 1}`}>Title</p>
-                {values.workexperience[i].title ? (
-                  <p key={`${i + 1}`}>{values.workexperience[i].title}</p>
-                ) : null}
-                <p key={`${i + 1}`}>Company</p>
-                {values.workexperience[i].company ? (
-                  <p key={`${i + 1}`}>{values.workexperience[i].company}</p>
-                ) : null}
-                <p key={`${i + 1}`}>Location</p>
-                {values.workexperience[i].location ? (
-                  <p key={`${i + 1}`}>{values.workexperience[i].location}</p>
-                ) : null}
-                <p key={`${i + 1}`}>From</p>
-                {values.workexperience[i].from ? (
-                  <p key={`${i + 1}`}>{values.workexperience[i].from}</p>
-                ) : null}
-                <p key={`${i + 1}`}>To</p>
-                {values.workexperience[i].to ? (
-                  <p key={`${i + 1}`}>{values.workexperience[i].to}</p>
-                ) : null}
-                <p key={`${i + 1}`}>Description</p>
-                {values.workexperience[i].descriptopn ? (
-                  <p key={`${i + 1}`}>{values.workexperience[i].descriptopn}</p>
-                ) : null}
-              </div>
-          ))
-          : null}
-      </div>
     </div>
   );
 };
--- src/containers/Profile/ProfileForm/render/RenderSummary.js
+++ src/containers/Profile/ProfileForm/render/RenderSummary.js
@@ -6,40 +6,30 @@ const renderSummary = (props) => {
   return (
     <div className={className}>
       <h5>{title}</h5>
-      {className === 'personal' && fields
+      {(className === 'personal'
+        || className === 'education'
+        || className === 'examplesofwork'
+        || className === 'experience')
+      && fields
         ? fields.map((item, i) => (
-            <div key={`${i + 1}`}>
-              <p key={`title${i + 1}`}>{item.title}</p>
-              <p key={`value${i + 1}`}>{item.value}</p>
+            <div key={`${item.title}${i}`}>
+              {item.map(object => (
+                <div key={`${object.title}${i}`}>
+                  <p key={`title${object.title}${i}`}>{object.title}</p>
+                  <p key={`value${object.title}${i}`}>{object.value}</p>
+                </div>
+              ))}
             </div>
         ))
         : null}
-      {className === 'education' && fields
-        ? fields.map((item, i) => (
-            <div key={`${i + 1}`}>
-              <p key={`title${i + 1}`}>{item.school.title}</p>
-              <p key={`value${i + 1}`}>{item.school.value}</p>
-              <p key={`title${i + 1}`}>{item.degree.title}</p>
-              <p key={`value${i + 1}`}>{item.degree.value}</p>
-              <p key={`title${i + 1}`}>{item.fieldofstudy.title}</p>
-              <p key={`value${i + 1}`}>{item.fieldofstudy.value}</p>
-              <p key={`title${i + 1}`}>{item.from.title}</p>
-              <p key={`value${i + 1}`}>{item.from.value}</p>
-              <p key={`title${i + 1}`}>{item.to.title}</p>
-              <p key={`value${i + 1}`}>{item.to.value}</p>
-              <p key={`title${i + 1}`}>{item.description.title}</p>
-              <p key={`value${i + 1}`}>{item.description.value}</p>
-            </div>
-        ))
-        : null}
-      {(className === 'languages'
-        || className === 'competences'
+      {(className === 'competences'
         || className === 'skills'
-        || className === 'methods')
+        || className === 'methods'
+        || className === 'languages')
       && fields ? (
         <ul>
-          {fields.map((item, i) => (
-            <li key={`value${i + 1}`}>{item.value}</li>
+          {fields.map(item => (
+            <li key={`value${item.title}`}>{item.value}</li>
           ))}
         </ul>
         ) : null}
@@ -50,7 +40,7 @@ const renderSummary = (props) => {
 renderSummary.propTypes = {
   className: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  fields: PropTypes.arrayOf({}),
+  fields: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.shape({}))),
 };
 
 export default renderSummary;
