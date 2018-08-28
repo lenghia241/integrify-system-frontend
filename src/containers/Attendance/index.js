aea24488261141b0ef1778a8cba9c30324cab9ad
ben <benminleung@gmail.com>
2018-08-28 13:58:20 +0300

    Class component implemented for Attendance page

--- src/containers/Attendance/index.js
+++ src/containers/Attendance/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PageTemplate from '../../components/PageTemplate';
 import StudentAttendance from '../../components/StudentAttendance';
 
-const Attendance = () => (
-  <PageTemplate heading="Attendance">
-    <div className="Attendance">
-      <StudentAttendance />
-    </div>
-  </PageTemplate>
-);
-
-export default Attendance;
+export default class Attendance extends Component {
+  render() {
+    const content = (
+      <PageTemplate heading="Attendance">
+        <div className="Attendance">
+          <StudentAttendance />
+        </div>
+      </PageTemplate>
+    );
+    return content;
+  }
+}
