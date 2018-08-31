176575b257f1f88b481f715bd10d4637839baa85
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-08-31 21:58:13 +0300

    Change persist user logic
    Instead of using cookie, let the backend decode the cookie from our API requests

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -1,8 +1,6 @@
 /*eslint-disable */
 export const START_FETCHING = 'START_FETCHING';
 
-export const FETCH_USER = 'FETCH_USER';
-
 // Login actions type
 export const AUTH_USER_SUCCESS = 'AUTH_USER_SUCCESS';
 export const AUTH_USER_FAIL = 'AUTH_USER_FAIL';
