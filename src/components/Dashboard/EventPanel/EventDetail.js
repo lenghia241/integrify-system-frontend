cbea0a2b8426c095f21e683d611eadef36672637
Abdi Muleta <abdi.muleta@gmail.com>
2018-08-31 21:03:10 +0300

    event details initial commit

---
+++ src/components/Dashboard/EventPanel/EventDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+const EventDetail = () => (
+  <div className = "event-detail">
+    <p>Detailed info of an event goes in here</p>
+  </div>
+);
+
+export default EventDetail;
---
+++ src/components/Dashboard/EventPanel/EventListDetailed.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+const EventListDetailed = () => (
+  <div className = "event-list-detailed">
+    <p>Detailed event list goes in here</p>
+  </div>
+);
+
+export default EventListDetailed;
