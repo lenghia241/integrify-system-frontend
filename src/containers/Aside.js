b6663bef509c0aefa1aafdbec780eaec2264d889
Phuoc Nguyen <phuoc.nguyen@metropolia.fi>
2018-08-31 22:03:36 +0300

    Aside container now have correct user props

--- src/containers/Aside.js
+++ src/containers/Aside.js
@@ -2,37 +2,26 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import { fetchUser as fetchUserAction, logOut as logOutAction } from '../store/actions';
+import { logOut as logOutAction } from '../store/actions';
 
 import { getAuth } from '../store/reducers';
 
 import AsideComponent from '../components/Aside';
 
-class Aside extends Component {
-  componentDidMount() {
-    const { fetchUser } = this.props;
-    fetchUser();
-  }
+const Aside = ({ user, logOut }) => <AsideComponent user={user} logOut={logOut} />;
 
-  render() {
-    const { auth, logOut } = this.props;
-    return <AsideComponent auth={auth} logOut={logOut} />;
-  }
-}
 Aside.propTypes = {
   logOut: PropTypes.func.isRequired,
-  auth: PropTypes.shape({}).isRequired,
-  fetchUser: PropTypes.func.isRequired,
+  user: PropTypes.shape({}).isRequired,
 };
 
 const mapStateToProps = state => ({
-  auth: getAuth(state),
+  user: getAuth(state).user,
 });
 
 export default connect(
   mapStateToProps,
   {
-    fetchUser: fetchUserAction,
     logOut: logOutAction,
   },
 )(Aside);
